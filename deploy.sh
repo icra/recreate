fc10ae0a5e02cb8982a53e1cbb3b9342d7005027
Lluís Bosch <holalluis@gmail.com>
2024-10-21 17:07:18 +0200

    update

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#git
+git add *
+git commit -am 'update'
+git push
+
+#desplega al servidor
+# -h  human readable format
+# -P  mostra progrés
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync -hPvr \
+  --exclude ".git" \
+  --exclude "deploy.sh" \
+  . root@icra.loading.net:/var/www/vhosts/icradev.cat/recreate.icradev.cat
